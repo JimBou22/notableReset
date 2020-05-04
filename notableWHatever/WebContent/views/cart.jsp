cecde22dbc09e9b6994a43b2d773b24e7f9acc4f
James Bou <Jbou422@gmail.com>
2020-05-04 15:52:40 -0700

    not using this lol

--- notableWHatever/WebContent/views/cart.jsp
+++ notableWHatever/WebContent/views/cart.jsp
@@ -29,12 +29,10 @@
             <tr class="cart_row">
               <td>
                 <form action="<c:url value='cart'/>" method="post">
-                   <input type="hidden" name="productId"
-                  			value="<c:out value='${item.product.productId}'/>">
+                   <%-- <input type="hidden" name="productId"
+                  			value="<c:out value='${item.product.productId}'/>"> --%>
                   <input type="hidden" name="name" 
                          value="<c:out value='${item.product.name}'/>">
-                  <input type="hidden" name="description"
-                  			value="<c:out value='${item.product.description}'/>">
                   <input type="hidden" name="price"
                   			value="<c:out value='${item.product.price}'/>">
                   <input type=text name="quantity" 
@@ -42,17 +40,15 @@
                   <input type="submit" name = "action" value="Update">
                 </form>                  
               </td>
-              <td>${item.product.name}</td>
+              <td colspan="3">${item.product.name}</td>
               <td>${item.product.priceCurrencyFormat}</td>
               <td>${item.totalCurrencyFormat}</td>
               <td>
                 <form action="<c:url value='cart'/>" method="post">
-                  <input type="hidden" name="productId"
-                  			value="<c:out value='${item.product.productId}'/>">
+                 <%--  <input type="hidden" name="productId"
+                  			value="<c:out value='${item.product.productId}'/>"> --%>
                   <input type="hidden" name="name" 
                          value="<c:out value='${item.product.name}'/>">
-                  <input type="hidden" name="description"
-                  			value="<c:out value='${item.product.description}'/>">
                   <input type="hidden" name="price"
                   			value="<c:out value='${item.product.price}'/>">
                   <input type="submit" name= "action" value="Remove">
@@ -61,12 +57,12 @@
             </tr>
           </c:forEach>
             <tr>
-              <td colspan="1">
+              <td colspan="3">
                 <p><b>To change the quantity for an item</b>, enter the new quantity 
                       and click on the Update button.</p>
                 <p><b>To remove an item</b>, click on the Remove button.</p>
               </td>
-              <td colspan="1">&nbsp;</td>
+              <td colspan="2">&nbsp;</td>
             </tr>
           </table>
       </c:otherwise>
--- notableWHatever/src/com/notable/controllers/OrderController.java
+++ notableWHatever/src/com/notable/controllers/OrderController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -88,13 +89,13 @@ public class OrderController extends HttpServlet {
 //        Product product = ProductDB.selectProduct(productCode);
         //Create product here with values from parameter.
         Product product = new Product();
-        int productId = Integer.parseInt(request.getParameter("productId"));
+       // int productId = Integer.parseInt(request.getParameter("productId"));
     	String name = request.getParameter("name");
-    	String desc = request.getParameter("description");
+    	
     	double price = Double.parseDouble(request.getParameter("price"));
-    	product.setProductId(productId);
+    	//product.setProductId(productId);
     	product.setName(name);
-    	product.setDescription(desc);
+    	
     	product.setPrice(price);    	
         if (product != null) {
             LineItem lineItem = new LineItem();
@@ -102,7 +103,42 @@ public class OrderController extends HttpServlet {
             lineItem.setProduct(product);
             cart.addItem(lineItem);
         }
-       
+        
+        
+        
+        //Add the item to the cookie
+        Cookie[] cookies = request.getCookies();
+		for (Cookie cookie : cookies) {
+			if (cookie.getName().equalsIgnoreCase("firstNameCookie")) { //find name
+				StringTokenizer values = new StringTokenizer(cookie.getValue(),"|"); //find cart cookie
+				values.nextToken();
+				if(values.hasMoreTokens()) {
+					String cartToken = values.nextToken();
+					for(Cookie temp  : cookies) {
+						if(temp.getName().equalsIgnoreCase(cartToken)) { //get cart cookie
+							temp.setValue(temp.getValue() + "|" +name+","+price); //add to cart cookie
+						}
+					}
+				} else { //cart cookie not yet made
+					String cartToken = cookie.getValue() + "|"+cookie.getValue()+"cart"; //give cartcookie name
+					cookie.setValue(cartToken); //set the value of the cookie to this new name
+					StringTokenizer temps = new StringTokenizer(cookie.getValue(),"|"); //get the values of the new cookie
+					temps.nextToken(); //skip first value
+					String cartCookie = temps.nextToken();
+					Cookie newCart = new Cookie(cartCookie,""); //make a new cart cookie
+					response.addCookie(newCart);
+					Cookie[] cookiesNew = request.getCookies(); //add stuff to new cart cookie
+					for(Cookie temp  : cookiesNew) {
+						System.out.println(temp.getName());
+						if(temp.getName().equalsIgnoreCase(cartCookie)) {
+							temp.setValue(temp.getValue() + "|" +name+"," +price);
+						}
+						System.out.println(temp.getName() +":" + temp.getValue());
+					}
+				}
+			}
+		}
+        
         session.setAttribute("cart", cart);
 //        List<LineItem> a = new ArrayList<LineItem>();
 //        a = cart.getItems();
@@ -129,13 +165,12 @@ public class OrderController extends HttpServlet {
         
 //        Product product = ProductDB.selectProduct(productCode);
         Product product = new Product();
-        int productId = Integer.parseInt(request.getParameter("productId"));
+//        int productId = Integer.parseInt(request.getParameter("productId"));
     	String name = request.getParameter("name");
-    	String desc = request.getParameter("description");
     	double price = Double.parseDouble(request.getParameter("price"));
-    	product.setProductId(productId);
+//    	product.setProductId(productId);
     	product.setName(name);
-    	product.setDescription(desc);
+    	
     	product.setPrice(price);  
         
         if (product != null && cart != null) {
@@ -155,13 +190,12 @@ public class OrderController extends HttpServlet {
         HttpSession session = request.getSession();
         Cart cart = (Cart) session.getAttribute("cart");
         Product product = new Product();
-        int productId = Integer.parseInt(request.getParameter("productId"));
+//        int productId = Integer.parseInt(request.getParameter("productId"));
     	String name = request.getParameter("name");
-    	String desc = request.getParameter("description");
     	double price = Double.parseDouble(request.getParameter("price"));
-    	product.setProductId(productId);
+//    	product.setProductId(productId);
     	product.setName(name);
-    	product.setDescription(desc);
+    	
     	product.setPrice(price);  
         if (product != null && cart != null) {
             LineItem lineItem = new LineItem();
@@ -170,119 +204,4 @@ public class OrderController extends HttpServlet {
         }
         return "/views/cart.jsp";
     }
-    
-	/*
-	 * private String checkUser(HttpServletRequest request, HttpServletResponse
-	 * response) {
-	 * 
-	 * HttpSession session = request.getSession(); User user = (User)
-	 * session.getAttribute("user");
-	 * 
-	 * // if the User object exists with address1, skip User page String url =
-	 * "/cart/user.jsp"; if (user != null && !user.getAddress1().equals("")) { url =
-	 * "/order/displayInvoice"; } else { // otherwise, check the email cookie
-	 * Cookie[] cookies = request.getCookies(); String email =
-	 * CookieUtil.getCookieValue(cookies, "emailCookie"); if (email.equals("")) {
-	 * user = new User(); url = "/cart/user.jsp"; } else { user =
-	 * UserDB.selectUser(email); if (user != null && !user.getAddress1().equals(""))
-	 * { url = "/order/displayInvoice"; } } } session.setAttribute("user", user);
-	 * return url; }
-	 */
-	/*
-	 * private String processUser(HttpServletRequest request, HttpServletResponse
-	 * response) {
-	 * 
-	 * String firstName = request.getParameter("firstName"); String lastName =
-	 * request.getParameter("lastName"); String companyName =
-	 * request.getParameter("companyName"); String email =
-	 * request.getParameter("email"); String address1 =
-	 * request.getParameter("address1"); String address2 =
-	 * request.getParameter("address2"); String city = request.getParameter("city");
-	 * String state = request.getParameter("state"); String zip =
-	 * request.getParameter("zip"); String country =
-	 * request.getParameter("country");
-	 * 
-	 * HttpSession session = request.getSession(); User user = (User)
-	 * session.getAttribute("user"); if (user == null) { user = new User(); }
-	 * 
-	 * if (UserDB.emailExists(email)) { user = UserDB.selectUser(email);
-	 * user.setFirstName(firstName); user.setLastName(lastName);
-	 * user.setEmail(email); user.setCompanyName(companyName);
-	 * user.setAddress1(address1); user.setAddress2(address2); user.setCity(city);
-	 * user.setState(state); user.setZip(zip); user.setCountry(country);
-	 * UserDB.update(user); } else { user.setFirstName(firstName);
-	 * user.setLastName(lastName); user.setEmail(email);
-	 * user.setCompanyName(companyName); user.setAddress1(address1);
-	 * user.setAddress2(address2); user.setCity(city); user.setState(state);
-	 * user.setZip(zip); user.setCountry(country); UserDB.insert(user); }
-	 * 
-	 * session.setAttribute("user", user);
-	 * 
-	 * return "/order/displayInvoice"; }
-	 */
-
-	/*
-	 * private String displayInvoice(HttpServletRequest request, HttpServletResponse
-	 * response) {
-	 * 
-	 * HttpSession session = request.getSession(); User user = (User)
-	 * session.getAttribute("user"); Cart cart = (Cart)
-	 * session.getAttribute("cart");
-	 * 
-	 * java.util.Date today = new java.util.Date();
-	 * 
-	 * Invoice invoice = new Invoice(); invoice.setUser(user);
-	 * invoice.setInvoiceDate(today); invoice.setLineItems(cart.getItems());
-	 * 
-	 * session.setAttribute("invoice", invoice);
-	 * 
-	 * return "/cart/invoice.jsp"; }
-	 */
-	/*
-	 * private String completeOrder(HttpServletRequest request, HttpServletResponse
-	 * response) {
-	 * 
-	 * HttpSession session = request.getSession(); Invoice invoice = (Invoice)
-	 * session.getAttribute("invoice");
-	 * 
-	 * String creditCardType = request.getParameter("creditCardType"); String
-	 * creditCardNumber = request.getParameter("creditCardNumber"); String
-	 * creditCardExpMonth = request.getParameter("creditCardExpirationMonth");
-	 * String creditCardExpYear = request.getParameter("creditCardExpirationYear");
-	 * 
-	 * User user = invoice.getUser(); user.setCreditCardType(creditCardType);
-	 * user.setCreditCardNumber(creditCardNumber);
-	 * user.setCreditCardExpirationDate(creditCardExpMonth + "/" +
-	 * creditCardExpYear);
-	 * 
-	 * // if a record for the User object exists, update it if
-	 * (UserDB.emailExists(user.getEmail())) { UserDB.update(user); } else { //
-	 * otherwise, write a new record for the user UserDB.insert(user); }
-	 * invoice.setUser(user);
-	 * 
-	 * // write a new invoice record InvoiceDB.insert(invoice);
-	 * 
-	 * // set the emailCookie in the user's browser. Cookie emailCookie = new
-	 * Cookie("emailCookie", user.getEmail());
-	 * emailCookie.setMaxAge(60*24*365*2*60); emailCookie.setPath("/");
-	 * response.addCookie(emailCookie);
-	 * 
-	 * // remove all items from the user's cart session.setAttribute("cart", null);
-	 * 
-	 * // send an email to the user to confirm the order. String to =
-	 * user.getEmail(); String from = "confirmation@freshcornrecords.com"; String
-	 * subject = "Order Confirmation"; String body = "Dear " + user.getFirstName() +
-	 * ",\n\n" + "Thanks for ordering from us. " +
-	 * "You should receive your order in 3-5 business days. " +
-	 * "Please contact us if you have any questions.\n" +
-	 * "Have a great day and thanks again!\n\n" + "Joe King\n" +
-	 * "Fresh Corn Records"; boolean isBodyHTML = false; try { MailUtil.sendMail(to,
-	 * from, subject, body, isBodyHTML); } catch(MessagingException e) { this.log(
-	 * "Unable to send email. \n" + "You may need to configure your system as " +
-	 * "described in chapter 15. \n" + "Here is the email you tried to send: \n" +
-	 * "=====================================\n" + "TO: " + to + "\n" + "FROM: " +
-	 * from + "\n" + "SUBJECT: " + subject + "\n" + "\n" + body + "\n\n"); }
-	 * 
-	 * return "/cart/complete.jsp"; }
-	 */   
 }
